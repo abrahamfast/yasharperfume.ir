a4a84b1ad45e246825f2773e4567cf78a59f05b5
abraham ghaemi <abrahamghaemi@gmail.com>
2020-08-26 02:24:27 +0430

    add new components and update Rel #1

---
+++ resources/views/components/preloader.blade.php
@@ -0,0 +1,7 @@
+    <div class="preloader-activate preloader-active">
+        <div class="preloader-area-wrap">
+            <div class="spinner d-flex justify-content-center align-items-center h-100">
+                <div class="img-loader"></div>
+            </div>
+        </div>
+    </div>
