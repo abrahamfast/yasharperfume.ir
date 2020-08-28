7f6b6ebdec95d37083814763ee16f1d112dbcb08
abraham ghaemi <ig.ghaemi@gmail.com>
2020-08-29 03:10:12 +0430

    fixed model name

--- app/View/Components/Products.php
+++ app/View/Components/Products.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 use Illuminate\Http\Request;
-use App\Products;
+use App\Products as ProductsModel;
 
 class Products extends Component
 {
@@ -16,7 +16,7 @@ class Products extends Component
      */
     public function __construct(Request $request)
     {
-        $this->products = Products::all();
+        $this->products = ProductsModel::all();
     }
 
     /**
