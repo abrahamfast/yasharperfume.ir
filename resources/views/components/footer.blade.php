a9bff480585ec4dceba67a0c94791e3544560275
abraham ghaemi <ig.ghaemi@gmail.com>
2020-08-30 04:15:32 +0430

    update navbar

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,16 +1,16 @@
         <footer>
             <div class="footer-nav-wrapper">
-                <a href="index.html" class="footer-nav-single">
+                <a href="/" class="footer-nav-single">
                     <div class="menu-wrapper">
                         <img src="/assets/img/icons/home.svg" class="injectable" alt="">
                     </div>
                 </a>
-                <a href="cart.html" class="footer-nav-single">
+                <a href="/cart" class="footer-nav-single">
                     <div class="menu-wrapper">
                         <img src="/assets/img/icons/cart.svg" class="injectable" alt="">
                     </div>
                 </a>
-                <a href="profile.html" class="footer-nav-single">
+                <a href="/profile" class="footer-nav-single">
                     <div class="menu-wrapper">
                         <img src="/assets/img/icons/profile.svg" class="injectable" alt="">
                     </div>
--- resources/views/components/products-area.blade.php
+++ resources/views/components/products-area.blade.php
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col-12">
                     <!-- section title -->
-                    <h2 class="section-title space-mb--20 text-right">جدیترین محصولات <a class="btn btn-dark btn-sm" href="#">همه محصولات</a></h2>
+                    <h2 class="section-title space-mb--20 text-right">جدیترین محصولات <a class="btn btn-dark btn-sm" href="/shop">همه محصولات</a></h2>
                     <!-- all products -->
                     <div class="all-products-wrapper space-mb-m--20">
                         <div class="row row-10">
@@ -12,7 +12,7 @@
 	                        <div class="col-6">
 	                            <div class="grid-product space-mb--20">
 	                                <div class="grid-product__image">
-	                                    <a href="/shop/{{ $product['url'].'/'.$product['id'] }}">
+	                                    <a href="/shop/{{ $product['id'].'/'.$product['url'] }}">
 	                                        <img src="{{ route('asset', $product['coverName']?? 'null' ) }}" class="img-fluid" alt="">
 	                                    </a>
 	                                    <button class="icon">
@@ -20,7 +20,7 @@
 	                                    </button>
 	                                </div>
 	                                <div class="grid-product__content">
-	                                    <h3 class="title"><a href="/shop/{{ $product['url'].'/'.$product['id'] }}">{{ $product['name'] }}</a></h3>
+	                                    <h3 class="title"><a href="/shop/{{ $product['id'].'/'.$product['url'] }}">{{ $product['name'] }}</a></h3>
 	                                    <span class="category">{{ $product['categoryName'] }}</span>
 	                                    <div class="price">
 	                                        <span class="main-price">{{ $product['costPriceConverted'] }}</span>
