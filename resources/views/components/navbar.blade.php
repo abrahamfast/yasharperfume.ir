ac8c353232d13501aba525589db48992ceac90f5
abraham ghaemi <ig.ghaemi@gmail.com>
2020-09-06 05:21:43 +0430

    update navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,34 +1,19 @@
         <div class="offcanvas-menu" id="offcanvas-menu">
-            @auth
-                <div class="profile-card text-center">
-                    <div class="profile-card__image space-mb--10">
-                        <img src="/assets/img/profile.jpg" class="img-fluid" alt="">
-                    </div>
-                    <div class="profile-card__content">
-                        <p class="name">Don Normane <span class="id">ID MD 4350</span></p>
-                    </div>
+            <div class="profile-card text-center">
+                <div class="profile-card__image space-mb--10">
+                    <img src="assets/img/profile.jpg" class="img-fluid" alt="">
                 </div>
-                @else
-                <div class="profile-card text-center">
-                    <div class="profile-card__content">
-                         <a class="btn btn-success" href="/register">ثبت نام</a>
-                    </div>
+                <div class="profile-card__content">
+                    <p class="name">Don Normane <span class="id">ID MD 4350</span></p>
                 </div>
-            @endauth
+            </div>
             <div class="offcanvas-navigation-wrapper space-mt--40">
                 <ul class="offcanvas-navigation">
-                    @auth
-                                            <li>
+                    <li>
                             <a href="/profile">پروفایل من</a>
                             <span class="icon"><img src="/assets/img/icons/profile-two.svg" class="injectable" alt=""></span>
                         </li>
 
-                    @else
-    <li>
-                            <a href="/login">لاگین</a>
-                            <span class="icon"><img src="/assets/img/icons/profile.svg" class="injectable" alt=""></span>
-                        </li>
-                    @endauth
                     <li>
                         <a href="/notification">پیام‌ها</a>
                         <span class="icon"><img src="/assets/img/icons/notification.svg" class="injectable" alt=""></span>
